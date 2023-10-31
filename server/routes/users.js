63037e9d3c80aa7272f0d07b63c93ead03140f83
Michał Pasynek <terabajt@terabajtlw.pl>
2023-10-31 11:32:24 +0100

    [BACKEND] Expansion of the User model with put and del methods

--- server/routes/users.js
+++ server/routes/users.js
@@ -18,7 +18,11 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-	const user = await User.findById(req.parrams.id).populate('invoices').select('-passwordHash');
+	const user = await User.findById(req.params.id)
+		.populate('invoices')
+		.populate({ path: 'invoices', populate: { path: 'entryItem' } })
+		.populate({ path: 'invoices', populate: { path: 'customer' } })
+		.select('-passwordHash');
 
 	if (!user) {
 		return res.status(500).json({ message: 'The user with that ID was not found! Try with correct ID.' });
@@ -43,7 +47,37 @@ router.post('/', async (req, res) => {
 	if (!user) return res.status(400).send('The user cannot be created.');
 	res.send(user);
 });
+router.put('/:id'),
+	async (req, res) => {
+		const userExist = await User.findById(req.params.id);
+		let newPassword;
 
+		if (req.body.password) {
+			newPassword = bcrypt.hashSync(req.body.password, 10);
+		} else {
+			newPassword = userExist.passwordHash;
+		}
+		const user = await User.findByIdAndUpdate(
+			req.params.id,
+			{
+				email: req.body.email,
+				name: req.body.name,
+				city: req.body.city,
+				address1: req.body.address1,
+				address2: req.body.address2,
+				zip: req.body.zip,
+				phone: req.body.phone,
+				isAdmin: req.body.isAdmin,
+				passwordHash: newPassword,
+			},
+			{
+				new: true,
+			}
+		);
+		if (!user) return res.status(400).send('The user cannot be updated!');
+
+		res.send(user);
+	};
 router.post('/login', async (req, res) => {
 	const user = await User.findOne({ email: req.body.email });
 	const secret = process.env.SECRET;
@@ -64,6 +98,20 @@ router.post('/login', async (req, res) => {
 	} else {
 		return res.status(500).send('Password is wrong');
 	}
+});
+
+router.delete('/:id', (req, res) => {
+	User.findByIdAndRemove(req.params.id)
+		.then(suer => {
+			if (user) {
+				return res.status(200).json({ success: true, message: 'The user is deleted.' });
+			} else {
+				return res.status(400).json({ success: false, message: 'USer not found!' });
+			}
+		})
+		.catch(err => {
+			return res.status(400).json({ success: false, error: err });
+		});
 });
 
 module.exports = router;
