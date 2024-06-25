29b94f41e3cde4aba6228f8c72fedbe0364241dc
Michał Pasynek <terabajt@terabajtlw.pl>
2024-06-25 10:07:25 +0200

    Set linter and eslint.

--- invoice2-team/environments/environment.development.ts
+++ invoice2-team/environments/environment.development.ts
@@ -2,6 +2,12 @@
 //     apiURL: 'http://localhost:3000/api/v1/'
 // };
 
+// export const environment = {
+//     apiURL: 'https://invoices2.up.railway.app/api/v1/'
+// };
+
+// https://mpvszkztaq.cfolks.pl/node/api/v1/invoices/statistics
+
 export const environment = {
-    apiURL: 'https://invoices2.up.railway.app/api/v1/'
+    apiURL: 'https://mpvszkztaq.cfolks.pl/api/v1/'
 };
--- invoice2-team/environments/environment.ts
+++ invoice2-team/environments/environment.ts
@@ -1,3 +1,3 @@
-export const environment = {
-    apiURL: 'http://localhost:3000/api/v1/'
-};
+// export const environment = {
+//     apiURL: 'http://localhost:3000/api/v1/'
+// };
