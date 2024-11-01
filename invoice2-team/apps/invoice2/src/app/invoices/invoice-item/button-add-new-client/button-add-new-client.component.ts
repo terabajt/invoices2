43f5cc39240c495826cdefa9ed96ab98320262f2
Michał Pasynek <terabajt@terabajtlw.pl>
2024-11-01 16:49:12 +0100

    refactor: Removed unused app

--- invoice2-team/apps/invoice2/src/app/invoices/invoice-item/button-add-new-client/button-add-new-client.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'invoice2-team-button-add-new-client',
-    templateUrl: './button-add-new-client.component.html',
-    styles: []
-})
-export class ButtonAddNewClientComponent {}
